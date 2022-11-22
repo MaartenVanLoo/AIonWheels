10b6218b068a09fa7f59b34d5abe36147fa8c1e3
Maarten Van Loo <95285436+MaartenVanLoo@users.noreply.github.com>
2022-11-22 23:30:08 +0100

    simple environment works

--- ReinforcementLearning/Environment.py
+++ ReinforcementLearning/Environment.py
@@ -1,5 +1,169 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+__initialDistance = 20
+
+
+def sigmoid(x):
+    if (x < -512): #avoid overflow of 'exp' function
+        return 0
+    return 1 / (1 + math.exp(-x))
+
+
+class DrivingCar:
+    def __init__(self, num_actions, speed, position, acceleration):
+        self.actions = np.linspace(-1, 1, num_actions)
+        self.position = position
+        self.speed = speed
+        self.acceleration = acceleration
+
+        self.target_speed = 20  # 20m/s = 72 km/h
+        self.maxBreaking = 3.5  # m/s²
+        self.maxThrottle = 5.5  # m/s²
+        self.dragArea = 0.8
+        self.airDensity = 1.204  # kg/m³
+        self.mass = 1000  # kg
+
+    def __str__(self) -> str:
+        string = "{"
+        string += "position: " + str(self.position) + ", "
+        string += "speed: " + str(self.speed) + ", "
+        string += "acceleration: " + str(self.acceleration) + "}"
+        return string
+
+    # def __init__(self, config:dict = None):
+    #    config = dict() if config is None else dict()
+    #    self.position = config.get("position", 0),
+    #    self.speed = config.get("speed", 0),
+    #    self.acceleration = config.get("acceleration", 0),
+
+    def getPos(self):
+        return self.position
+
+    def getSpeed(self):
+        return self.speed
+
+    def updatePos(self, dt):
+        self.position += self.speed * dt
+
+    def updateSpeed(self, dt):
+        self.speed += self.acceleration * dt
+
+    def updateAcceleration(self, action=None, dt=1):
+        # compute force from action
+        if action is None:
+            force = 0.05
+        else:
+            force = self.actions[action]
+            if (force < 0):
+                # break force
+                force *= self.maxBreaking
+            else:
+                # throttle
+                force *= self.maxThrottle
+
+        # compute force from drag
+        drag = self.dragArea * self.speed * self.airDensity / (2 * self.mass) * self.speed
+        self.acceleration = force - drag
+
+    def printStats(self):
+        print("speed: ", self.speed / 3.6)
+        print("position: ", self.position)
+        print("acceleration: ", self.acceleration)
+
+    def step(self, action=None, dt=1):
+        self.updateAcceleration(action, dt)
+        self.updateSpeed(dt)
+        self.updatePos(dt)
+
+
 class Environment:
+    def __init__(self, config: dict):
+        self.config = config;
+        self.agent_config = config.get("agent", dict())
+        self.car_config = config.get("car", dict())
+        self.agent = DrivingCar(7, 0, 0, 45)
+        self.car = DrivingCar(7, 50, 200, 0)
 
-    def __init__(self) -> None:
-        super().__init__()
+        self.stepCount = 0;
+        self.history = {'agent': [], 'car': []};
 
+    def __str__(self) -> str:
+        string = ""
+        string += "Agent:\t" + str(self.agent) + "\n"
+        string += "Car  :\t" + str(self.car) + "\n"
+        string += "State:\t" + str(self.__getState()) + "\n"
+        return string
+
+    def step(self, action):
+        self.car.step(dt=0.05)
+        self.agent.step(action=action, dt=0.05)
+        self.stepCount += 1
+
+        self.history['agent'].append(self.agent.getPos())
+        self.history['car'].append(self.car.getPos())
+
+        return self.__getState(), self.__getReward(), self.__isTerminal(), self.__info()
+
+    def reset(self):
+        self.agent = DrivingCar(
+            num_actions=self.config.get("num_actions", 7),
+            position=self.agent_config.get("position", 0),
+            speed=self.agent_config.get("speed", 0),
+            acceleration=self.agent_config.get("acceleration", 0))
+        self.car = DrivingCar(
+            num_actions=self.config.get("num_actions", 7),
+            position=self.agent_config.get("position", 20),
+            speed=self.agent_config.get("speed", 5),
+            acceleration=self.agent_config.get("acceleration", 0))
+
+        self.stepCount = 0
+        self.history = {'agent': [], 'car': []}
+
+        return self.__getState()
+
+    def __getState(self):
+        """
+        Compute the state of the current environment
+        :return:
+        """
+        distance = self.car.getPos() - self.agent.getPos()
+        return distance, self.agent.getSpeed()
+
+    def __getReward(self):
+        """
+        Compute the state of the last step
+        """
+        #TODO: think about the reward function
+        distance = self.car.getPos() - self.agent.getPos()
+        reward = 1 / math.sqrt(distance);  # closer = better
+
+        dv = self.agent.target_speed - self.agent.getSpeed()  # slower = higher dv
+        reward += sigmoid(-dv)-.5
+        return reward
+
+    def __isTerminal(self):
+        """
+        Return true when a terminal state is reached
+        """
+        distance = self.car.getPos() - self.agent.getPos()
+        return self.agent.position > 2000 or distance < 4 or self.stepCount > 4000
+
+    def __info(self):
+        info = dict()
+        if self.stepCount > 4000:
+            info['TimeLimit.truncated'] = True
+        return info
+
+    def printCars(self):
+        print("Agent:")
+        self.agent.printStats()
+        print("Car:")
+        self.car.printStats()
+
+    def plot(self):
+        x = np.linspace(0, self.stepCount - 1, num=self.stepCount)
+        plt.plot(x, self.history['agent'])
+        plt.plot(x, self.history['car'])
+        plt.show()
--- ReinforcementLearning/Qlearner.py
+++ ReinforcementLearning/Qlearner.py
@@ -5,11 +5,40 @@ import torch
 import math
 import numpy as np
 from Environment import Environment
+import matplotlib
+import matplotlib.pyplot as plt
 
+class DQN(torch.nn.Module):
 
-class DQN():
-    pass
+    def __init__(self, config) -> None:
+        super().__init__()
+        self.device = torch.device(config.get('device','cuda') if torch.cuda.is_available() else 'cpu')
+        self.num_inputs=config.get('num_inputs',2)
+        self._hidden = config.get('hidden',[128, 128])
+        self.num_actions=config.get('num_actions',7)
 
+        modules =[]
+        modules.append(torch.nn.Linear(self.num_inputs, self._hidden[0]))
+        modules.append(torch.nn.ReLU())
+        for i in range(0,len(self._hidden)-1):
+            modules.append(torch.nn.Linear(self._hidden[i], self._hidden[i+1]))
+            modules.append(torch.nn.ReLU())
+        modules.append(torch.nn.Linear(self._hidden[len(self._hidden)-1], self.num_actions))
+
+        self.model = torch.nn.Sequential(*modules)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            with torch.no_grad():
+                state = torch.FloatTensor(state).to(self.device).unsqueeze(0)
+            q_value = self.forward(state)
+            action = q_value.max(1)[1].data[0].cpu().numpy().tolist()  # argmax over actions
+        else:
+            action = random.randint(0, self.num_actions - 1)
+        return action
 
 class _ReplayBuffer:
     def __init__(self, capacity):
@@ -40,11 +69,11 @@ class _ReplayBuffer:
 
 class Qlearner:
     def __init__(self, environment, DQN, config) -> None:
-        self.device = torch.device(config.get('device','gpu') if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device(config.get('device','cuda') if torch.cuda.is_available() else 'cpu')
 
         self.env = environment
-        self.targetDQN = DQN().to(self.device)
-        self.currentDQN = DQN().to(self.device)
+        self.targetDQN = DQN(config).to(self.device)
+        self.currentDQN = DQN(config).to(self.device)
 
         # training parameters
         self.batch_size = config.get('batch_size',32)
@@ -59,22 +88,41 @@ class Qlearner:
 
     def train(self):
         episode_reward = 0
+        losses = []
+        all_rewards = []
+
+        state = self.env.reset()
         for frame_idx in range(1, self.num_frames + 1):
             epsilon = self.__epsilon_by_frame(frame_idx)
             action = self.currentDQN.act(state, epsilon)
 
             next_state, reward, done, info = self.env.step(action);
 
-            loss = self.__computeLoss(state, action, reward, next_state, done)
+            print(env)
+            print(done)
+            if not 'TimeLimit.truncated' in info.keys() or not info['TimeLimit.truncated']:
+                self.__replay_buffer.push(state, action, reward, next_state, done)
 
             state = next_state
             episode_reward += reward
 
+            if (len(self.__replay_buffer) > self.batch_size):
+                loss = self.__computeLoss()
+                #loss = loss.data.cpu().numpy().tolist()
+                #losses.append(loss)
+
+
+
             if done:
+                env.plot()
                 state = self.env.reset()
+                all_rewards.append(episode_reward)
                 episode_reward = 0
-            if frame_idx % 200 == 0:
-                pass  # plot if required
+            if frame_idx % 2000 == 0:
+                # plot if required
+                plt.plot(all_rewards)
+                plt.show()
+                pass
             # update target every 100 frames
             if frame_idx % 100 == 0:
                 self.__update_target()
@@ -97,14 +145,14 @@ class Qlearner:
     def __update_target(self):
         self.targetDQN.load_state_dict(self.currentDQN.state_dict())
 
-    def __computeLoss(self, state, action, reward, next_state, done):
+    def __computeLoss(self):
         state, action, reward, next_state, done = self.__replay_buffer.sample(self.batch_size)
 
-        state = torch.from_numpy(state).to(self.device)
-        next_state = torch.from_numpy(next_state).to(self.device)
-        action = torch.from_numpy(action).to(self.device)
-        reward = torch.from_numpy(reward).to(self.device)
-        done = torch.from_numpy(done).to(self.device)
+        state = torch.FloatTensor(np.float32(state)).to(self.device)
+        next_state = torch.FloatTensor(np.float32(next_state)).to(self.device)
+        action = torch.LongTensor(action).to(self.device)
+        reward = torch.FloatTensor(reward).to(self.device)
+        done = torch.BoolTensor(done).to(self.device)
 
 
         q_values = self.currentDQN.forward(state)
@@ -126,13 +174,17 @@ class Qlearner:
 if __name__ == "__main__":
     #config = optional, default values have been set in the qlearning framework
     config = {
-        'device':'gpu',
-        'batch_size':32,
-        'num_frames':20000,
+        'device':'cuda',
+        'batch_size':64,
+        'num_frames':200000,
         'gamma':0.99,
-        'replay_size':50000
+        'replay_size':50000,
+
+        'num_input':2, #=size of states!
+        'num_actions':71,
+        'hidden':[128,128],
     }
-    env = Environment()
+    env = Environment(config)
     qlearning = Qlearner(env, DQN, config)
     qlearning.train()
     qlearning.save("TrainedModel.save")
