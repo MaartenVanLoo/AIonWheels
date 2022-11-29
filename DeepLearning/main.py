3efa8e4be9c26b0105aaa5bbbe439378ec134e63
muntan <serdar_tst@hotmail.com>
2022-11-29 14:04:49 +0100

    waitfortick

--- DeepLearning/main.py
+++ DeepLearning/main.py
@@ -223,10 +223,10 @@ def main():
 
         while True:
             clock.tick()
-            # if args.sync:
-            world.tick()
-            # else:
-            #     world.wait_for_tick()
+        # if args.sync:
+            # world.tick()
+        # else:
+            world.wait_for_tick()
 
             world.tick(clock)
             world.render(display)
