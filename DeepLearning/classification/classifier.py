2d25d00e6edc833c58d272f17692902244576edd
muntan <serdar_tst@hotmail.com>
2022-12-01 17:52:03 +0100

    functioning classifier for now with working wandb

--- DeepLearning/classification/classifier.py
+++ DeepLearning/classification/classifier.py
@@ -2,80 +2,57 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import wandb
+import numpy as np
 
 wandb.login() #  To be able to log metrics to projects
 
+batches = []
+
+for i in range(6):
+    batches.append(2**(i+2))
+
 sweep_config = {
-    'method': 'random'  # Select each new combination at random
+    'project' : 'classification-test',
+    'entity' : 'projectcarla',
+    'method': 'random',
+    'metric' : {
+        'name': 'loss',
+        'goal': 'minimize',
+    },
+    'parameters' : {
+        'epochs': {
+            'value': 10,
+            },
+        'lr': {
+            'min': 0.00001,
+            'max': 0.01,
+            },
+        'batch_size': {
+            'values' : batches,
+            },
     }
-
-metric = {
-'name': 'loss',
-'goal': 'minimize'   
 }
+sweep_id = wandb.sweep(sweep_config)
 
-sweep_config['metric'] = metric
-
-parameters_dict = { 
-    'epochs': {
-        'values': 5
-        },
-    'learning_rate': {
-        'distribution': 'uniform',
-        'min': 0.00001,
-        'max': 0.1
-        },
-    'batch_size': {
-        'values': 8
-        },
-    'optimizer': {
-        'values': ['adam', 'sgd']
-        },
-    }
-
-sweep_config['parameters'] = parameters_dict
-
-sweep_id = wandb.sweep(sweep_config, project="classification-test", entity="projectcarla")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") #Testing whether a GPU (cuda device) is available. If there is one, use this, if not use the CPU.
+print(device)
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-
 trainset = torchvision.datasets.CIFAR10(root='./DeepLearning/classification/data', train=True,
                                         download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./DeepLearning/classification/data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                         shuffle=False, num_workers=2)
+# trainloader = torch.utils.data.DataLoader(trainset, batch_size=16,
+#                                         shuffle=True, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-# functions to show an image
-
-
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = next(dataiter)
-
 import torch.nn as nn
 import torch.nn.functional as F
-
 
 class Net(nn.Module):
     def __init__(self):
@@ -97,78 +74,56 @@ class Net(nn.Module):
         return x
 
 
-net = Net()
-
 import torch.optim as optim
+def train(config = None):
+    with wandb.init(config=config): #initialize a new wandb run
+        config = wandb.config
 
-with wandb.init(config=config): #initialize a new wandb run
-    config = wandb.config
+        net = Net().to(device)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=config.learning_rate)
+        # trainset = torchvision.datasets.CIFAR10(root='./DeepLearning/classification/data', train=True,
+        #                             download=True, transform=transform)
 
-
-    for epoch in range(config.epochs):  # loop over the dataset multiple times
-
-        running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data
-
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = net(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            wandb.log({"loss": loss, "epoch": epoch})
-
-            # # Optional
-            # wandb.watch(net)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=config.batch_size,
+                                            shuffle=True, num_workers=2)
 
 
-            # print statistics
-            running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                running_loss = 0.0
-
-    print('Finished Training')
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.SGD(net.parameters(), lr=config.lr, momentum=0.9)
 
 
-PATH = './DeepLearning/classification/cifar_net.pth'
-torch.save(net.state_dict(), PATH)
+        for epoch in range(config.epochs):  # loop over the dataset multiple times
+
+            running_loss = 0.0
+            for i, data in enumerate(trainloader, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+                inputs, labels = inputs.to(device), labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                # # Optional
+                # wandb.watch(net)
+
+                # print statistics
+                running_loss += loss.item()
+                if i % 2000 == 1999:    # print every 2000 mini-batches
+                    wandb.log({"loss": loss, "epoch": epoch})
+                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                    running_loss = 0.0
+
+        print('Finished Training')
 
 
-dataiter = iter(testloader)
-images, labels = next(dataiter)
-
-# print images
-imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
-
-net = Net()
-net.load_state_dict(torch.load(PATH))
-
-outputs = net(images)
-
-correct = 0
-total = 0
-# since we're not training, we don't need to calculate the gradients for our outputs
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        # calculate outputs by running images through the network
-        outputs = net(images)
-        # the class with the highest energy is what we choose as prediction
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
-
-wandb.log(outputs)
+        PATH = './DeepLearning/classification/cifar_net.pth'
+        torch.save(net.state_dict(), PATH)
 
 
-print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+wandb.agent(sweep_id, train, count=10)
