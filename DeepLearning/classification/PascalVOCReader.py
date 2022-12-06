24b6998aecc395b0f4a06ea7dc9d50cccaedd7ef
Max8274 <95033890+Max8274@users.noreply.github.com>
2022-12-06 11:31:11 +0100

    Add pascalVOC file reader function

---
+++ DeepLearning/classification/PascalVOCReader.py
@@ -0,0 +1,21 @@
+import xml.etree.ElementTree as ET
+
+
+def read_content(xml_file: str):
+
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    bbox_coordinates = []
+    for member in root.findall('object'):
+        class_name = member[0].text  # class name
+
+        # bbox coordinates
+        xmin = int(member[4][0].text)
+        ymin = int(member[4][1].text)
+        xmax = int(member[4][2].text)
+        ymax = int(member[4][3].text)
+        # store data in list
+        bbox_coordinates.append([class_name, xmin, ymin, xmax, ymax])
+
+    return bbox_coordinates
