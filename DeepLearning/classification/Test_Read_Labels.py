5a08aa262f1ac9ebcbe9871b6e53267342033802
Max8274 <95033890+Max8274@users.noreply.github.com>
2022-12-08 17:15:57 +0100

    add comment

--- DeepLearning/classification/Test_Read_Labels.py
+++ DeepLearning/classification/Test_Read_Labels.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
             bbox_coordinates = read_content("data/"+xml)
             print(bbox_coordinates)
 
+            #we add the image to the list with his labels
+            #can also be placed in the read_content function if necessary
             image_name = xml.replace(".xml", ".png")
             image_path = "data/"+image_name
             bbox_coordinates.insert(0, [image_path])
