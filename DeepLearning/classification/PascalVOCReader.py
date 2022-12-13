d3871a06b9549b1e287185a050336744beaebda7
Max8274 <95033890+Max8274@users.noreply.github.com>
2022-12-13 11:23:36 +0100

    PASCALVOC to YOLO format

--- DeepLearning/classification/PascalVOCReader.py
+++ DeepLearning/classification/PascalVOCReader.py
@@ -9,9 +9,29 @@ def read_content(xml_file: str):
 
     print("xml_file:", xml_file)
 
+    for member in root.findall('size'):
+        image_width = float(member[0].text)
+        image_height = float(member[1].text)
+
     bbox_coordinates = []
     for member in root.findall('object'):
         class_name = member[0].text  # class name
+
+        if class_name == 'car':
+            class_id = 1
+        elif class_name == 'motorcycle':
+            class_id = 2
+        elif class_name == 'bike':
+            class_id = 3
+        elif class_name == 'police':
+            class_id = 4
+        elif class_name == 'ambulance':
+            class_id = 5
+        elif class_name == 'firetruck':
+            class_id = 6
+        else:
+            class_id = 7 # truck
+
 
         # bbox coordinates
         xmin = float(member[4][0].text)
@@ -19,7 +39,18 @@ def read_content(xml_file: str):
         xmax = float(member[4][2].text)
         ymax = float(member[4][3].text)
         # store data in list
-        bbox_coordinates.append([class_name, xmin, ymin, xmax, ymax])
+
+        width = xmax - xmin
+        height = ymax - ymin
+        x_center = xmin + width/2
+        y_center = ymin + height/2
+
+        norm_width = width / image_width
+        norm_height = height / image_height
+        norm_x_center = x_center / image_width
+        norm_y_center = y_center / image_height
+
+        bbox_coordinates.append([class_id, norm_x_center, norm_y_center, norm_width, norm_height])
 
     return bbox_coordinates
 
