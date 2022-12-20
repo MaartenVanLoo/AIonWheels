2410f1a4f707a3760a93dec6b7e11bced26d8fd7
OliverRommens <oliverrommens1@gmail.com>
2022-12-20 17:47:51 +0100

    final

--- FinalIntegration/DeepLearningRecognition/DeepLearningRecognition.py
+++ FinalIntegration/DeepLearningRecognition/DeepLearningRecognition.py
@@ -1,8 +1,9 @@
 import os
 import torch
 
+
 class DeepLearningRecognition(object):
-    def __init__(self, carlaWorld, config = None) -> None:
+    def __init__(self, carlaWorld, config=None) -> None:
         if not config:
             config = dict()
         super().__init__()
@@ -11,33 +12,41 @@ class DeepLearningRecognition(object):
         self._agent = carlaWorld.getPlayer()
 
         self.device = torch.device(self.config.get('device', 'cuda') if torch.cuda.is_available() else 'cpu')
-        self._model = self._load_model("")
+        self._model = self._load_model(config.get("model_path",""), config.get("hubconf_path",""))
         self._model_name = "/"
 
         self.detections = None
         self.detected_image = None
 
     def detect(self):
-        #load image from sensor
-        sensor = self.carlaWorld.get_sensor("Camera")
+        # Load image from sensor
+        sensor = self._carlaWorld.get_sensor("Camera")
+
         if sensor is None:
             return
         image = sensor.getState()
         if image is None:
             return
 
-        tensor = torch.tensor(image).to(self.device)
+        tensor = torch.tensor(image.copy()).to(self.device)
+        tensor = tensor.unsqueeze(0).permute(0, 3, 1, 2)
         output = self._model.forward(tensor)
 
         self.detections = output
         self.detected_image = output
 
-    def _load_model(self, filename):
+    def _load_model(self, filename, hubconf_path):
         if not (os.path.exists(filename) and os.path.isfile(filename)):
+            print("Model file not found")
             return None
-
-        #model = torch.load(filename)
-        #return model
+        model = torch.hub.load(hubconf_path, 'custom', path=filename, source='local')
+        return model
 
     def getModelName(self) -> str:
         return self._model_name
+
+
+
+
+
+
