fef46584f46feb31bd3e4d386e7f065761cc4869
Max8274 <95033890+Max8274@users.noreply.github.com>
2022-12-22 14:49:04 +0100

    updated code to read labels

--- DeepLearning/classification/PascalVOCReader.py
+++ DeepLearning/classification/PascalVOCReader.py
@@ -1,13 +1,9 @@
 import xml.etree.ElementTree as ET
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 
 def read_content(xml_file: str):
     tree = ET.parse(xml_file)
     root = tree.getroot()
-
-    #print("xml_file:", xml_file)
 
     for member in root.findall('size'):
         image_width = float(member[0].text)
@@ -53,31 +49,3 @@ def read_content(xml_file: str):
         bbox_coordinates.append([class_id, norm_x_center, norm_y_center, norm_width, norm_height])
 
     return bbox_coordinates
-
-
-def print_content(info_list: list):
-    img = plt.imread(info_list[0][0])
-    #text = ""
-    #for i in range(1, len(info_list)):
-        #text = text + str(info_list[i][0])
-        #text = "[{} , {} , {}, {}, {}]".format(info_list[i][0], info_list[i][1], info_list[i][2], info_list[i][3], info_list[i][4])
-
-    """
-        if i == 0:
-            img = mpimg.imread(info_list[i][0])
-            
-            print("Image:", info_list[i][0])
-        else:
-            print("Type:", info_list[i][0],
-                  "\nCoordinates:",
-                  "\n\txmin:", info_list[i][1],
-                  "\n\tymin:", info_list[i][2],
-                  "\n\txmax:", info_list[i][3],
-                  "\n\tymax:", info_list[i][4],"\n")
-                  """
-
-    #plt.text(i*20, 0, text, fontsize=8, bbox=dict(fill=False, edgecolor='white', linewidth=5))
-    plt.imshow(img)
-    plt.show()
-    plt.pause(5)
-    plt.close()
