03dd4b554a00e6b51d97a3b8f78a5bbc37be5bde
MaartenVanLoo <95285436+MaartenVanLoo@users.noreply.github.com>
2022-12-25 19:13:52 +0100

    try to use multiprocessing

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
         'debug':False,
     }
     dl_recognition_config = {
-        'model_path':os.path.dirname(os.path.abspath(__file__)) + "/FinalIntegration/models/weights.pt",
+        'model_path':os.path.dirname(os.path.abspath(__file__)) + "/FinalIntegration/models/best22_12.pt",
         'hubconf_path':os.path.dirname(os.path.abspath(__file__)) + "/FinalIntegration/DeepLearningRecognition",
         'conf_threshold':0.25, #minimum confidence for object to be detected
         'iou_threshold':0.45,
